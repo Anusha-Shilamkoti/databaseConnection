30cdbab236e57ec31897ae51662a83527d1f0769
Anusha-Shilamkoti <60070323+Anusha-Shilamkoti@users.noreply.github.com>
2020-01-28 17:58:08 +0530

    Initial commit

---
+++ src/databaseConnection/an.java
@@ -0,0 +1,22 @@
+package databaseConnection;
+
+import java.sql.*;
+
+public class an {
+
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+			// here sonoo is database name, root is username and password
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from employee");
+			while (rs.next())
+				System.out.println(
+						rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
